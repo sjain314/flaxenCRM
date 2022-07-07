5c7d8deed9bc28fbdc811a10dfad2e5444fccc4f
sjain314 <shrutijain14233@gmail.com>
2022-07-07 15:31:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/emcrm/EmployeeRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.emcrm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmployeeRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_registration);
+    }
+}
